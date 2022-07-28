4164b0a007b49ed0fad6d3cb5c7dc5cfd4488ba7
N Healy <n.healy@haberda.sh>
2022-07-28 20:05:09 +0100

    Cleanup - replacing autogenerated placeholders

---
+++ RedRiver-DrinksMachine/Controllers/DrinksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedRiver_DrinksMachine.Controllers
+{
+    public class DrinksController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
